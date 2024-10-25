45b0ba3cc3149531a90e780638ddf43712201280
msa507 <msa10101962@gmail.com>
2024-10-25 19:04:22 +0500

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,24 @@
+
+
+def send_email(message, recipient, *sender):
+    sender_non = (input("Адрес отправителя: "))
+    if '@' in recipient and sender and sender_non:
+        if '.com' or '.net' or '.ru' in recipient and sender and sender_non:
+            if sender_non == recipient:
+                print("Нельзя отправить самому себе!")
+            elif sender_non == sender:
+                print(f"Письмо успешно отправлено с адреса {sender_non} на адрес {recipient}.")
+            else:
+                print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ!"
+                      f"Письмо успешно отправлено с адреса {sender_non} на адрес {recipient}.")
+        else:
+            print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+    else:
+        print("Это не e-mail адрес!")
+
+
+recipient = (input("Получатель сообщения: "))
+message = (input("Введите текст сообщения: "))
+sender = "university.help@gmail.com"
+
+send_email(message, recipient, sender)
